0f376616ef0223b379d3a8f0bfbf9f2b0a69b342
kajikawa256 <kajikawa2003@gmail.com>
2023-10-11 15:11:48 +0900

    [update] 馬データを取り出しやすいように整形

--- php/insert.php
+++ php/insert.php
@@ -4,6 +4,8 @@ $fullpath="/Users/2220077/AppData/Local/Programs/Python/Python311/python ";
 $command= $fullpath . "../python/result.py";
 exec($command,$output);
 
-var_dump($output)
+$json = mb_convert_encoding($output, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
+$result_list = json_decode($json[0], true);
+var_dump($result_list)
 
 ?>
--- python/result.py
+++ python/result.py
@@ -1,6 +1,7 @@
+import json
 import scrayping
 
-print(scrayping.scrayping())
+print(json.dumps(scrayping.scrayping()))
 
 # for x in range(9):
 #   print() #レースID #日付 #レース名 #時刻 #距離 #頭数 #馬場(芝かだーとか) #開催地 #天気
