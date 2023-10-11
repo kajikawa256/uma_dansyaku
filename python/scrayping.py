f375e3670e21729bdce7f0e0af8e3fd6aa9adad2
kajikawa256 <kajikawa2003@gmail.com>
2023-10-11 21:22:38 +0900

    [update] レーステーブルの項目をすべて取得できるように更新

--- python/scrayping.py
+++ python/scrayping.py
@@ -1,22 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
-import re
-import scrayping
+# import re
+# import scrayping
 import time
 from datetime import datetime, timedelta
 import constant as con
 
 def scrayping():
-  #現在の日付を取得
-  current_date = datetime.now()
-
   #変数宣言
-  year = "2023"
-  race_id_list = [[[]]]  #[開催場,開催回,何日目][レース番号][馬番(着順)]
-  race_id = ""
-  error = "true"
-  result_list = []
-  result_ranking = {}
+  year = "2023"        #任意の年数
+  race_id = ""         #宣言
+  error = "true"       #エラーフラグ
+  result_list = []     #result_rankingの子要素
+  result_ranking = {}  #total_infoの子要素
+  resulut_pay = {}     #total_infoの子要素
+  race_info = {}       #total_infoの子要素
+  total_info = []      #すべてまとめたリスト
 
   #指定した年から現在の日付までの全レースを取得
   for where in con.race_venue_list:
@@ -24,10 +23,13 @@ def scrayping():
     for howtime in range(con.HOWTIME):
       for howdays in range(con.HOWDAYS):
         for race_num in range(con.RACENUM):
+
           #文字に変換
           ht = str(howtime+1)
           hd = str(howdays+1)
           rn = str(race_num+1)
+
+          #1桁の数字を0付きの2桁の数字に変換
           race_id = year + where + ('0' + ht if len(ht) != 2 else ht) + ('0' + hd if len(hd) != 2 else hd) + ('0' + rn if len(rn) != 2 else rn)
 
           #URLを作成
@@ -37,6 +39,8 @@ def scrayping():
           res = requests.get(url)
           res.encoding = "EUC-JP"
           soup = BeautifulSoup(res.text, "html.parser")
+
+          
           list = soup.find_all("table")
           title = soup.find_all("title")
 
@@ -80,4 +84,6 @@ def scrayping():
       if error == "false" :
         break
     
+
+scrayping()
 
