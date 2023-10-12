77590bb3f8b6c30f0ed0bdcbe3ba4501f2868f8f
Shibata-8bit <sbtbnnsk0712@gmail.com>
2023-10-12 14:39:07 +0900

    レースIDとレース毎のTOP5を出力できるようになった

--- php/insert.php
+++ php/insert.php
@@ -1,11 +1,20 @@
 <?php
-
-$fullpath="/Users/2220077/AppData/Local/Programs/Python/Python311/python ";
-$command= $fullpath . "../python/result.py";
-exec($command,$output);
+$command = "C:/Users/2220200/AppData/Local/Programs/Python/Python312/python ../python/result.py";
+exec($command, $output);
 
 $json = mb_convert_encoding($output, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
 $result_list = json_decode($json[0], true);
-var_dump($result_list)
+$result_raceid = array_keys($result_list);
+// var_dump($result_list);
 
-?>
+$count = 0;
+for ($j = 0; $j < count($result_raceid); $j++) {
+  foreach ($result_list as $id) {
+    echo "レースID" . $result_raceid[$j] . "\n";
+    for ($i = 0; $i < count($id); $i += 2) {
+      echo "馬番" . $id[$i] . "\n";
+      echo "馬名" . $id[$i + 1] . "\n";
+    }
+    echo "\n";
+  }
+}
