900c4f4f041ef5bf559ad07eeae73af7c17657ac
kajikawa256 <kajikawa2003@gmail.com>
2023-10-12 20:49:17 +0900

    [update] スクレイピング処理を項目別に別ファイル化、関数化して可読性と可用性の向上

---
+++ python/component/result_list.py
@@ -0,0 +1,32 @@
+#呼び出すときにBeautifulSoupで整形したhtmlを引数として渡す
+import constant as con
+
+#全馬の全データを返す関数
+def get(soup):
+  result_list = []
+
+  table = soup.find_all("tr",class_="HorseList")
+
+  #race_listに格納
+  for x in table:
+    print(x.text.split())
+  
+  return result_list
+
+
+#上位５馬の馬枠と馬番と馬名を返す関数
+def get_only(soup):
+  result_list = []
+
+  table = soup.find_all("tr",class_="HorseList")
+
+  #race_listに格納
+  for x in range(5):
+    datas = table[x].text.split()
+    
+    #馬枠、馬番、馬名の順
+    result_list.append(datas[con.HORSE_FRAME])
+    result_list.append(datas[con.HORSE_NUM])
+    result_list.append(datas[con.HORSE_NAME])
+  
+  return result_list
--- python/scrayping.py
+++ python/scrayping.py
@@ -1,89 +1,89 @@
 import requests
 from bs4 import BeautifulSoup
-# import re
-# import scrayping
+import re
 import time
 from datetime import datetime, timedelta
 import constant as con
+import component.result_list as rl
+import component.race_info as ri
 
-def scrayping():
-  #変数宣言
-  year = "2023"        #任意の年数
-  race_id = ""         #宣言
-  error = "true"       #エラーフラグ
-  result_list = []     #result_rankingの子要素
-  result_ranking = {}  #total_infoの子要素
-  resulut_pay = {}     #total_infoの子要素
-  race_info = {}       #total_infoの子要素
-  total_info = []      #すべてまとめたリスト
 
-  #指定した年から現在の日付までの全レースを取得
-  for where in con.race_venue_list:
-    error = "true"
-    for howtime in range(con.HOWTIME):
-      for howdays in range(con.HOWDAYS):
-        for race_num in range(con.RACENUM):
 
-          #文字に変換
-          ht = str(howtime+1)
-          hd = str(howdays+1)
-          rn = str(race_num+1)
+#変数宣言
+year = "2023"        #任意の年数
+race_id = ""         #宣言
+error = "true"       #エラーフラグ
+result_list = []     #result_rankingの子要素
+race_list = []       #race_infoの子要素
+pay_list = []        #result_payの子要素
+result_ranking = {}  #total_infoの子要素
+race_info = {}       #total_infoの子要素
+resulut_pay = {}     #total_infoの子要素
+total_info = []      #すべてまとめたリスト
 
-          #1桁の数字を0付きの2桁の数字に変換
-          race_id = year + where + ('0' + ht if len(ht) != 2 else ht) + ('0' + hd if len(hd) != 2 else hd) + ('0' + rn if len(rn) != 2 else rn)
+#指定した年から現在の日付までの全レースを取得
+for where in con.race_venue_list:
+  error = "true"
+  for howtime in range(con.HOWTIME):
+    for howdays in range(con.HOWDAYS):
+      for race_num in range(con.RACENUM):
 
-          #URLを作成
-          url = f"https://race.netkeiba.com/race/result.html?race_id={race_id}&rf=race_list"
+        #文字に変換
+        ht = str(howtime+1)
+        hd = str(howdays+1)
+        rn = str(race_num+1)
 
-          #スクレイピング
-          res = requests.get(url)
-          res.encoding = "EUC-JP"
-          soup = BeautifulSoup(res.text, "html.parser")
+        #1桁の数字を0付きの2桁の数字に変換
+        race_id = year + where + ('0' + ht if len(ht) != 2 else ht) + ('0' + hd if len(hd) != 2 else hd) + ('0' + rn if len(rn) != 2 else rn)
 
-          
-          list = soup.find_all("table")
-          title = soup.find_all("title")
+        #URLを作成
+        url = f"https://race.netkeiba.com/race/result.html?race_id={race_id}&rf=race_list"
 
-          #正常にスクレイピングが行われた場合
-          if list != []:
-            list = list[0].text
-            list = list.split()
-            title = title[0].text
-            #1秒待つ
-            time.sleep(0.1)
+        #スクレイピング
+        res = requests.get(url)
+        res.encoding = "EUC-JP"
+        soup = BeautifulSoup(res.text, "html.parser")
+        
+        list = soup.find_all("table")
+        title = soup.find_all("title")
 
-            # #上位５馬とレースタイトル表示
-            z = 14
-            for x in range (5):
-              for y in range (2):
-                if x == 0:
-                  # print(f"{list[3 + z + y]}",end=' ')
-                  result_list.append(list[3 + z + y])
-                else:
-                  # print(f"{list[2 + z + y]}",end=' ')
-                  result_list.append(list[2 + z + y])
-              z+=15
+        #正常にスクレイピングが行われた場合（ここの条件式は変更の余地あり）
+        if list != []:
 
-            #改行
-            # print()
-            # print()
+          result_list = rl.get_only(soup) #上位５馬の馬枠、馬番、馬名を取得
+          race_list = ri.get(soup)        #レース情報を取得
 
-            result_ranking[race_id] = result_list
+          #1秒待つ
+          time.sleep(1)
 
-            # print(result_ranking)
-            # print(result_list)
+          #各辞書型の配列にリストを格納する
+          result_ranking[race_id] = result_list
+          race_info[race_id] = race_list
+          resulut_pay[race_id] = pay_list
 
-          else:
-            error = "false"
-            break
-          result_list = []
+          print(result_list)
+          print(race_list)
 
-        if error == "false" :
+          # print(result_ranking)
+          # print(race_info)
+
+        else:
+          error = "false"
           break
-        return result_ranking
+        #親要素に追加したら初期化
+        result_list = []
+        race_list = []
+        pay_list = []
+        print()
       if error == "false" :
         break
-    
+    if error == "false" :
+      break
 
-scrayping()
+#スクレイピングしたデータをすべてtotal_infoに格納
+total_info.append(result_ranking)
+total_info.append(race_info)
+total_info.append(resulut_pay)
+
+
 
