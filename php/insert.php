c16b514061e4b00e254783e528b0356d9332d1b3
Shibata-8bit <sbtbnnsk0712@gmail.com>
2023-10-13 14:39:37 +0900

    結果情報テーブルにデータを登録する配列を作成

--- php/insert.php
+++ php/insert.php
@@ -5,16 +5,28 @@ exec($command, $output);
 $json = mb_convert_encoding($output, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
 $result_list = json_decode($json[0], true);
 $result_raceid = array_keys($result_list);
-// var_dump($result_list);
 
-$count = 0;
-for ($j = 0; $j < count($result_raceid); $j++) {
+// $count = 0;
+// for ($j = 0; $j < count($result_raceid); $j++) {
+//   foreach ($result_list as $id) {
+//     echo "レースID" . $result_raceid[$j] . "\n";
+//     for ($i = 0; $i < count($id); $i += 2) {
+//       echo "馬番" . $id[$i] . "\n";
+//       echo "馬名" . $id[$i + 1] . "\n";
+//       echo "馬枠" . $id[$i + 2] . "\n";
+//     }
+//     echo "\n";
+//   }
+// }
+
+// 結果情報テーブル
+$result_information = [];
+for ($i = 0; $i < count($result_raceid); $i++) {
   foreach ($result_list as $id) {
-    echo "レースID" . $result_raceid[$j] . "\n";
-    for ($i = 0; $i < count($id); $i += 2) {
-      echo "馬番" . $id[$i] . "\n";
-      echo "馬名" . $id[$i + 1] . "\n";
-    }
-    echo "\n";
+    for ($j = 0; $j = count($id); $j += 3) {
+      $result_information[$i] = "('" . $result_raceid[$i] . "','" . $id[$j] . "'," . $id[$j + 1] . "," . $id[$j + 2] . "," . $id[$j + 3] . ")";
+    };
   }
 }
+
+echo $result_information;
