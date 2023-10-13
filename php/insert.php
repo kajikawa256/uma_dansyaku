f7b1b62399f85b2671558c05947b36a1d1a39c75
Shibata-8bit <sbtbnnsk0712@gmail.com>
2023-10-13 17:51:32 +0900

    １開催単位での取得可能になった(Pythonファイルもいじった)

--- php/insert.php
+++ php/insert.php
@@ -5,21 +5,28 @@ exec($command, $output);
 $json = mb_convert_encoding($output, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
 $result_list = json_decode($json[0], true);
 $result_raceid = array_keys($result_list);
-// var_dump($result_list);
+// var_dump($result_list[0]);
+/*
+  配列メモ
+  １開催ずつ取れる
+  $result_list[何日目][何レース目][0がレース結果,0+1がレース情報,0+2が払い戻し][詳細]
+*/
+
+var_dump($result_list[1]);
 
 // 結果情報テーブル
-$result_information = [];
-for ($i = 0; $i < count($result_raceid); $i++) {
-  foreach ($result_list as $id) {
-    for ($j = 0; $j = count($id); $j += 45) {
-      // $result_information[$i] = "('" . $result_raceid[$i] . "','" . $id[$j] . "'," . $id[$j + 1] . "," . $id[$j + 2] . "," . $id[$j + 3] . ")";
-      echo $result_raceid[$i];
-      // echo $id[$j];
-      // echo $id[$j + 1];
-      // echo $id[$j + 2];
-      // echo $id[$j + 3];
-    };
-  }
-}
+// $result_information = [];
+// for ($i = 0; $i < count($result_raceid); $i++) {
+//   foreach ($result_list as $id) {
+//     for ($j = 0; $j = count($id); $j += 45) {
+//       // $result_information[$i] = "('" . $result_raceid[$i] . "','" . $id[$j] . "'," . $id[$j + 1] . "," . $id[$j + 2] . "," . $id[$j + 3] . ")";
+//       echo $result_raceid[$i];
+//       // echo $id[$j];
+//       // echo $id[$j + 1];
+//       // echo $id[$j + 2];
+//       // echo $id[$j + 3];
+//     };
+//   }
+// }
 
 // echo $result_information;
--- python/scrayping.py
+++ python/scrayping.py
@@ -9,97 +9,95 @@ import component.pay_list as pl
 
 
 def scrayping():
-  #変数宣言
-  year = "2023"        #任意の年数
-  race_id = ""         #宣言
-  error = "true"       #エラーフラグ
-  result_list = []     #result_rankingの子要素
-  race_list = []       #race_infoの子要素
-  pay_list = []        #result_payの子要素
-  result_ranking = []  #total_infoの子要素
-  race_info = []       #total_infoの子要素
-  resulut_pay = []      #total_infoの子要素
-  race_total = []
-  total_info = []      #すべてまとめたリスト
+    # 変数宣言
+    year = "2023"  # 任意の年数
+    race_id = ""  # 宣言
+    error = "true"  # エラーフラグ
+    result_list = []  # result_rankingの子要素
+    race_list = []  # race_infoの子要素
+    pay_list = []  # result_payの子要素
+    result_ranking = []  # total_infoの子要素
+    race_info = []  # total_infoの子要素
+    resulut_pay = []  # total_infoの子要素
+    race_total = []
+    total_info = []  # すべてまとめたリスト
+    one_race = []    # 1レースごと
+    one_day = []     # 一日のレース情報
+    one_open_day = []  # 開催ごと
 
+    # 指定した年から現在の日付までの全レースを取得
+    for where in con.race_venue_list:
+        error = "true"
+        for howtime in range(con.HOWTIME):
+            for howdays in range(con.HOWDAYS):
+                for race_num in range(con.RACENUM):
 
+                    # 文字に変換
+                    ht = str(howtime+1)
+                    hd = str(howdays+1)
+                    rn = str(race_num+1)
 
-  #指定した年から現在の日付までの全レースを取得
-  for where in con.race_venue_list:
-    error = "true"
-    for howtime in range(con.HOWTIME):
-      for howdays in range(con.HOWDAYS):
-        for race_num in range(con.RACENUM):
+                    # 1桁の数字を0付きの2桁の数字に変換
+                    race_id = year + where + ('0' + ht if len(ht) != 2 else ht) + (
+                        '0' + hd if len(hd) != 2 else hd) + ('0' + rn if len(rn) != 2 else rn)
 
-          #文字に変換
-          ht = str(howtime+1)
-          hd = str(howdays+1)
-          rn = str(race_num+1)
+                    # URLを作成
+                    url = f"https://race.netkeiba.com/race/result.html?race_id={race_id}&rf=race_list"
 
-          #1桁の数字を0付きの2桁の数字に変換
-          race_id = year + where + ('0' + ht if len(ht) != 2 else ht) + ('0' + hd if len(hd) != 2 else hd) + ('0' + rn if len(rn) != 2 else rn)
+                    # スクレイピング
+                    res = requests.get(url)
+                    res.encoding = "EUC-JP"
+                    soup = BeautifulSoup(res.text, "html.parser")
 
-          #URLを作成
-          url = f"https://race.netkeiba.com/race/result.html?race_id={race_id}&rf=race_list"
+                    list = soup.find_all("table")
 
-          #スクレイピング
-          res = requests.get(url)
-          res.encoding = "EUC-JP"
-          soup = BeautifulSoup(res.text, "html.parser")
-          
-          list = soup.find_all("table")
+                    # 正常にスクレイピングが行われた場合（ここの条件式は変更の余地あり）
+                    if list != []:
 
-          #正常にスクレイピングが行われた場合（ここの条件式は変更の余地あり）
-          if list != []:
+                        # 上位５馬の馬枠、馬番、馬名を取得しresult_listに格納
+                        result_list = rl.get_only(soup, race_id)
+                        # レース情報を取得しrace_listに格納
+                        race_list = ri.get(soup, race_id)
+                        # 払い戻し情報を取得しpay_listに格納
+                        pay_list = pl.get(soup, race_id)
 
-            result_list = rl.get_only(soup,race_id) #上位５馬の馬枠、馬番、馬名を取得しresult_listに格納
-            race_list = ri.get(soup,race_id)        #レース情報を取得しrace_listに格納
-            pay_list = pl.get(soup,race_id)         #払い戻し情報を取得しpay_listに格納
+                        one_race.append(result_list)
+                        one_race.append(race_list)
+                        one_race.append(pay_list)
 
-            #1秒待つ
-            # time.sleep(1)
+                        # 1秒待つ
+                        # time.sleep(1)
 
-            #各辞書型の配列にリストを格納する
-            result_ranking.append(result_list)
-            race_info.append(race_list)
-            resulut_pay.append(pay_list)
+                        # 親要素に追加したら初期化
+                        result_list = []
+                        race_list = []
+                        pay_list = []
 
-            #親要素に追加したら初期化
-            result_list = []
-            race_list = []
-            pay_list = []
-            
-          else:
-            error = "false"
-            break
+                    else:
+                        error = "false"
+                        break
+                    # -----race_num-----
+                    one_day.append(one_race)
+                    one_race = []    # one_raceを初期化
+                if error == "false":
+                    break
+                # -----howdays-----
 
-        if error == "false" :
-          break
+                one_open_day.append(one_day)
+                one_day = []      # one_dayを初期化
 
-        race_total.append(result_ranking)
-        race_total.append(race_info)
-        race_total.append(resulut_pay)
+                # return(one_open_day)
+                # print(one_open_day)
 
-        # print(race_total)
+            return(one_open_day)
+            if error == "false":
+                break
 
-        # return(race_total)
+            # １開催分
+            return(one_open_day)
+            # -----howtime-----
 
-        result_ranking = [] 
-        race_info = []      
-        resulut_pay = []
+        # 1開催場分
+        # return(total_info)
 
-        total_info.append(race_total)
-    
-      if error == "false" :
-        break
-      race_total = []
-      #１開催分
-      return(total_info)
-    
-    #1開催場分
-    return(total_info)  
-      
-  # return(total_info)
-
-
-
+    # return(total_info)
