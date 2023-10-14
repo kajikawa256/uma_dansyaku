26a5ced6dc383db21fcb3ff455e24376b7f29e2f
2220054 <2220054@ecc.ac.jp>
2023-10-14 12:48:44 +0900

    ・レース番号の型をVARCHARの変更 ・INSERT文の挿入テスト

--- php/connect.php
+++ php/connect.php
@@ -15,23 +15,24 @@ function insert($table,$column,$value){
 
         //文字列結合
         $columns = implode(',',$column);
-        $values = ':' . implode(', :', $value);
+        $values = implode(',',$value);
+        // $values = ':' . implode(', :', $value);
         //空白削除
         $columns = str_replace(' ', '', $columns);
         $columns = str_replace('　', '', $columns);
         $values = str_replace(' ', '', $values);
         $values = str_replace('　', '', $values);
         
-        //テスト
-        var_dump($columns);
-        var_dump($values);
+        // //テスト
+        // var_dump($columns);
+        // var_dump($values);
 
         //sql作成
         $sql =  "INSERT INTO $table($columns)VALUES($values)";
-
+        echo $sql;
         // ステートメントの準備
         $stmt = $db -> prepare($sql);
-        echo "実行成功";
+        // echo "実行成功";
 
         // カラム名と値のバインド（関連付ける）
         $stmt->bindParam(1, $table);
@@ -48,9 +49,9 @@ function insert($table,$column,$value){
 }
 
 // テスト用データ
-$table = "example_table";
-$columns = ['column1', 'column2', 'column3'];
-$values = ['value1', 'value2', 'value3'];
+$table = "RACE";
+$columns = ['RACE_ID', 'RACEDATE','RNAME','RACENUMBER','TIME','DISTANCE','HORSE_TOTAL','GROUND','PLACE','WEATHER','SPIN','SITUATION'];
+$values = ['2003090901', date('Y-m-d'), '2歳未勝利','10R',date('H:i:s'),1800,14,'芝','札幌','晴れ','右','重'];
 
 // insert() 関数を呼び出す
 $result = insert($table, $columns, $values);
