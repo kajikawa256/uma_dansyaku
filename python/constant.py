c66682072c65aed6e07d0a5c8d8232f5f80d06a9
Shiba-8bit <sbtbnnsk0712@gmail.com>
2023-10-26 15:00:08 +0900

    続き：次回SQL文に入れれるようにデータの形を整形から

--- python/constant.py
+++ python/constant.py
@@ -33,3 +33,25 @@ race_venue_list = [
   HANSHIN,
   OGURA
 ]
+
+
+'''
+-----------------------以下データベース-----------------------
+'''
+
+# テーブル情報をまとめたリスト
+TABLE = ['HIT','RACE','RESULT_HORSE','PREDICTION_HORSE','HIT_DETAIL']
+COLUMNS = [
+    ['RACEDATE','COLLECT_PR','HIT_PR'],
+    ['RACE_ID', 'RACEDATE','RNAME','RACENUMBER','TIME','DISTANCE','HORSE_TOTAL','GROUND','PLACE','WEATHER','SPIN','SITUATION'],
+    ['RACE_ID', 'HNAME', 'HORSENUMBER', 'HORSEFRAME', 'RANKING'],
+    ['RACE_ID', 'HNAME', 'HORSENUMBER', 'HORSEFRAME', 'RANKING'],
+    ['RACE_ID','KINDS','HORSENUMBER','HORSEFRAME','BETBACK','POPULAR']
+]
+
+# テーブルを明示的に指定できるようにした定数
+HIT = 0
+RACE = 1
+RESULT_HORSE = 2
+PREDICTION_HORSE = 3
+HIT_DETAIL = 4
---
+++ python/insert.py
@@ -0,0 +1,40 @@
+# 他ファイル呼び出し
+import constant as cons
+
+# module呼び出し
+import MySQLdb as db
+
+def insert(table, datalist:list) :
+
+    # SQL文に入れれるようにデータの形を整形
+    SQLdatalist
+    for data in datalist :
+        if type(data) is str :
+            SQLdatalist = data
+
+    try:
+        # データベース接続情報
+        dbconnect = db.connect(           
+            user = "hoge",
+            passwd = "hogehoge",
+            host = "hoge",
+            db = "mysql"
+        )
+
+        # カーソルを取得する
+        cur = dbconnect.cursor()           
+
+        # SQL（データベースを操作するコマンド）を実行する
+        sql = f"INSERT INTO {cons.TABLE[table]} VALUES('')"
+        cur.execute(sql)
+
+        dbconnect.commit()
+        
+
+
+    except Exception as e:
+        print(f"Error Occurred: {e}")
+
+    finally:
+        cur.close
+        dbconnect.close()
