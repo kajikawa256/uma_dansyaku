4dd9daa7a226802c4ff2635a2675296c966706a5
kajikawa256 <kajikawa2003@gmail.com>
2024-01-26 15:25:12 +0900

    [add]ファイル追加

--- python-container/python/create_dataset.py
+++ python-container/python/create_dataset.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
 from tqdm import tqdm
-import os
+# import os
 import datetime
-import re
+# import re
 from tqdm._tqdm_notebook import tqdm_notebook
 from sklearn import preprocessing
 import classes.convert_df as con
@@ -17,56 +17,50 @@ class Dataset:
         self.__columnsDict = self.__generateColumns()
         self.__encodeEnvs = self.__generateEncodeEnvs()
         if load:
-            self.__dataset = self.__updateDataset()
+            self.__dataset = pd.read_pickle('./data/dataset.pkl')
     @property
     def dataset(self):
-        return self.__dataset[self.__columnsDict['all']].copy()
+        return self.__dataset[self.columnsDict['all']].copy()
     @property
     def columnsDict(self):
         return self.__columnsDict.copy()
     # エンコード実行メソッド
     def encoding(self, src, fit=False):
-        # データセットをコピー
-        df = src.fillna(0).copy()
+        df = src.copy()
         for key, env in tqdm(self.__encodeEnvs.items()):
             cols = env['cols']
-            ordinal_encoder = preprocessing.OrdinalEncoder(
-                handle_unknown="use_encoded_value",
-                unknown_value=-1,
-                )
-            #le = ordinal_encoder
             le = env['encoder']
             if fit:
                 # fitの指示があったらデータフレームから対象の値を取り出して実行
                 na = df[cols].to_numpy()
-                tg = na.reshape(-1,1).tolist()
+                tg = na.reshape(-1).tolist()
                 le.fit(tg)
-                #le = ld.transform(tg)
             for col in cols:
                 # 欠損データ以外の列を取り出す
                 notNull = df[col][df[col].notnull()]
                 # エンコード実行してindexをキーにデータフレームに書き込む
-                #df[col] = pd.Series(le.transform(tg))
                 df[col] = pd.Series(le.transform(notNull), index=notNull.index)
                 # エンコードした列はcategory列に変換
                 df[col] = df[col].astype('category')
         cols = self.__columnsDict['numeric']
         for col in cols:
+            df[col] = (df[col].replace('中止',np.nan).replace('除外',np.nan)
+                       .replace('取消',np.nan).replace('失格',np.nan).replace('未定',np.nan))
             df[col] = df[col].astype(float)
 
         return df
     # 出馬表用前処理メソッド
     def preprocessingEntryTable(self, entryDf): 
         df = self.addHistrical(entryDf)
-        df = self.addInterval(df)
+        # df = self.addInterval(df)
         df = df[self.__columnsDict['entry_table']]
         df = self.encoding(df)
         return df
     # データセット一括更新メソッド
     def update(self):
         print('更新します')
-        import cron.scrayping
-        import cron.prediction
+        # import cron.scrayping
+        #import cron.prediction
 
         self.__dataset = self.__updateDataset()
     # 列名生成メソッド
@@ -74,27 +68,27 @@ class Dataset:
         # 使用する列名を指定
         resultCol = [
             'RACEDATE', 'RACE_ID', 'HORSEFRAME', 'HORSENUMBER', 'HORSE_ID', 'GENDER', 'AGE',
-            'WEIGHT', 'JOCKEY_ID', 'ODDS', 'POPULAR', 'BASE',
-            'HORSE_WEIGHT', 'WEIGHT_GAIN_LOSS', '出走間隔', 'HANDICAP', 'RANKING','RACENUMBER',
-            'GROUND', 'SPIN', 'DISTNCE', 'WEATHER', 'SITUATION', 'PLACE',
-            'GRADE', 'HANDICAP', #頭数消してる
+            'WEIGHT', 'JOCKEY_ID', 'ODDS', 'POPULAR', 'TORAINER_ID', 'BASE',
+            'HORSE_WEIGHT', 'WEIGHT_GAIN_LOSS', 'HANDICAP', 'RANKING','RACENUMBER',# 'interval', 
+            'GROUND', 'SPIN', 'DISTANCE', 'WEATHER', 'SITUATION', 'PLACE',
+            'GRADE', 'LIMIT','HORSE_TOTAL','TIME'
         ]
-        recordCol = [
-            'RACENUMBER', 'HORSE_TOTAL', 'HORSEFRAME', 'HORSENUMBER', 'ODDS', 'POPULAR', 'RANKIN',
-            'jockeyId', 'WEIGHT', 'HORSE_WEIGHT',
-            'WEIGHT_GAIN_LOSS', '出走間隔', 'GROUND', 'SPIN', 'DISTANCE',
-            'WEATHER', 'SITUATION', 'PLACE', 'GRADE', 'LIMIT', 'HANDICAP'
-        ]
-        # 前N走分戦績の列名を生成
-        recordCol9 = []
-        for i in range(1, 10):
-            tmpList = list(map(lambda x: x + '_' + str(i), recordCol))
-            recordCol9 += tmpList
+        # recordCol = [
+        #     'RACENUMBER', 'HORSE_TOTAL', 'HORSEFRAME', 'HORSENUMBER', 'ODDS', 'POPULAR', 'RANKING',
+        #     'JOCKEY_ID', 'WEIGHT', 'TIME', 'HORSE_WEIGHT',
+        #     'WEIGHT_GAIN_LOSS', 'GROUND', 'SPIN', 'DISTANCE', #interval,
+        #     'WEATHER', 'SITUATION', 'PLACE', 'GRADE', 'LIMIT', 'HANDICAP',
+        # ]
+        # # 前N走分戦績の列名を生成
+        # recordCol9 = []
+        # for i in range(1, 10):
+        #     tmpList = list(map(lambda x: x + '_' + str(i), recordCol))
+        #     recordCol9 += tmpList
 
         # 生成した列名を辞書に格納
         columnsDict = {}
-        columnsDict['all'] = resultCol + recordCol9
-        columnsDict['histrical'] = recordCol9
+        columnsDict['all'] = resultCol# + recordCol9
+        # columnsDict['histrical'] = recordCol9
         l = columnsDict['all'].copy()
         l.remove('RANKING')
         columnsDict['entry_table'] = l
@@ -102,8 +96,8 @@ class Dataset:
         # 量的変数の列名を生成
         numericCols = ['AGE']
         cols1 = ['HORSEFRAME', 'HORSENUMBER', 'ODDS', 'POPULAR', 'WEIGHT',
-                'HORSE_WEIGHT', 'WEIGHT_GAIN_LOSS', '出走間隔', 'RACENUMBER', 'GROUND']
-        cols2 = ['HORSE_TOTAL', 'RANKING']
+                'HORSE_WEIGHT', 'WEIGHT_GAIN_LOSS', 'RACENUMBER', 'GROUND',]# 'interval',]
+        cols2 = ['HORSE_TOTAL', 'RANKING', 'TIME']
         numericCols += cols1
         cols3 = cols1 + cols2
         for i in range(1, 10):
@@ -121,25 +115,15 @@ class Dataset:
         encodeEnvs = {}
         # horseIdだけは勝手が違うので個別に環境を生成
         dd = {}
-        dd['cols'] = (['horseId'])
-        
-        # dd['encoder'] = preprocessing.OrdinalEncoder(
-        #     handle_unknown="use_encoded_value",
-        #     unknown_value=-1,
-        #     )
+        dd['cols'] = ['HORSE_ID']
         dd['encoder'] = preprocessing.LabelEncoder()
-        encodeEnvs['horseId'] = dd
+        encodeEnvs['HORSE_ID'] = dd
         # それ以外の対象はリスト化してfor文で処理
-        cols = ['GENDER', 'BASE', 'JOCKEY_ID', 'HANDICAP', 'GROUND',
+        cols = ['GENDER', 'TORAINER_ID', 'BASE', 'JOCKEY_ID', 'HANDICAP', 'GROUND',
                 'SPIN', 'WEATHER', 'SITUATION', 'PLACE', 'GRADE', 'LIMIT']
         for col in cols:
             dd = {}
             dd['cols'] = [s for s in self.__columnsDict['all'] if col in s]
-            
-            # dd['encoder'] = preprocessing.OrdinalEncoder(
-            #     handle_unknown="use_encoded_value",
-            #     unknown_value=-1,
-            # )
             dd['encoder'] = preprocessing.LabelEncoder()
             encodeEnvs[col] = dd
         return encodeEnvs
@@ -155,18 +139,21 @@ class Dataset:
             print('レース結果に戦績データを付与します')
             # 上記のデータに戦績データを付与
             dataAddHis = Dataset.addHistrical(dataDf)
-            print(dataAddHis)
             if len(loadDf) >= 0:
                 dataAddHis = pd.concat([loadDf, dataAddHis], axis=0)
         else:
             dataAddHis = loadDf
         
-        print('出走間隔を計算し列に追加します')
-        dataAddInterval = Dataset.addInterval(dataAddHis)
-        dataset = (dataAddInterval
-                    .sort_values(['RACEDATE', 'RACE_ID', 'HORSENUMBER'], ascending=[True, True, True])
-                    .reset_index(drop=True))
-        
+        # print('interval(出走間隔)を計算し列に追加します')
+        # dataAddInterval = Dataset.addInterval(dataAddHis)
+        # dataset = (dataAddInterval
+        #             .sort_values(['RACEDATE', 'RACE_ID', 'HORSENUMBER'], ascending=[True, True, True])
+        #             .reset_index(drop=True))
+        dataset = dataAddHis
+        print('データセットをファイルに保存します')
+        dataset.to_pickle('./data/dataset_no.pkl')
+        print('データセットの更新が完了しました')
+
         return dataset
     # レース結果とレース情報をファイルからロード
     def __generateRaceResult(self):
@@ -196,26 +183,8 @@ class Dataset:
             if '_i' not in cn:
                 columns.append(cn)
         dfHorseResulti = df[columns]
-        ##追加
-        dfHorseResulttt = pd.merge(left=srcdf, right=dfHorseResulti,
-                        how='left', on='RACE_ID', suffixes=['', '_i'])
-        df = dfHorseResulttt.copy()
-        columns = []
-        for cn in df.columns:
-            if '_i' not in cn:
-                columns.append(cn)
-        dfHorseResulttt = df[columns]
-        columns = ['RACE_ID', 'HORSEFRAME', 'HORSENUMBER', 'HORSE_ID', 'HNAME', 'GENDER', 'AGE', 'WEIGHT', 'JOCKEY',
-            'JOCKEY_ID', 'ODDS', 'POPULAR', 'TORAINER', 'BASE', 'HORSE_WEIGHT', 'WEIGHT_GAIN_LOSS', 'RANKING',
-            'RNAME', 'RACENUMBER', 'GROUND', 'SPIN', 'DISTANCE', 'WEATHER',
-            'SITUATION', 'RACEDATE', 'PLACE', 'GRADE', 'LIMIT', 'HANDICAP','HORSE_TOTAL']
-            #賞金
-        dfHorseResulttt = dfHorseResulttt[columns]
-        dfHorseResulttt = dfHorseResulttt[~dfHorseResulttt[['HORSE_ID', 'RACEDATE']].duplicated()]
-        ###追加
-
         # 引数のデータフレームをコピー
-        df = dfHorseResulttt.copy()     #もともとはsrcdf
+        df = srcdf.copy()
         # 戦績データとする列を_1～_9のサフィックスを付与して一度numpy配列にする
         cols = np.array([dfHorseResulti.add_suffix(f'_{i}').columns for i in range(1, 10)])
         # 列名を1次元配列にreshape
@@ -243,21 +212,21 @@ class Dataset:
         df[cols] = df.progress_apply(generateHistoricalData, axis=1, result_type='expand')
 
         return df
-    # レース結果または出馬表データの出走間隔を計算するメソッド
-    @staticmethod
-    def addInterval(srcdf):
-        df = srcdf.copy()
-        # スカラー関数の定義
-        def calcInterval(x):
-            try:
-                interval = (datetime.datetime.strptime(x['RACEDATE'], '%Y/%m/%d') - 
-                            datetime.datetime.strptime(x['RACEDATE_1'], '%Y/%m/%d'))
-                return interval.days
-            except:
-                return 0
-        # apply関数で一括処理
-        df['出走間隔'] = df.progress_apply(calcInterval, axis=1)
-        return df
+    # レース結果または出馬表データのinterval(出走間隔)を計算するメソッド
+    # @staticmethod
+    # def addInterval(srcdf):
+    #     df = srcdf.copy()
+    #     # スカラー関数の定義
+    #     def calcInterval(x):
+    #         try:
+    #             interval = (datetime.datetime.strptime(x['RACEDATE'], '%Y年%m月%d日') - 
+    #                         datetime.datetime.strptime(x['RACEDATE_1'], '%Y年%m月%d日'))
+    #             return interval.days
+    #         except:
+    #             return 0
+    #     # apply関数で一括処理
+    #     df['interval'] = df.progress_apply(calcInterval, axis=1)
+    #     return df
 def version():
     mejor = 0
     minor = 1
